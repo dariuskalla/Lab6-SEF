3626eef2017010a3c3757e50284bea85be4348a9
Darius Calapis <dariuskalla@gmail.com>
2020-04-10 18:19:58 +0300

    updating my stuff

--- src/loose/oose/fis/lab/student/manager/Main.java
+++ src/loose/oose/fis/lab/student/manager/Main.java
@@ -1,12 +1,12 @@
-package sample;
+package loose.oose.fis.lab.student.manager;
 
 public class Main extends Controller {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
+        primaryStage.setTitle("FIS - Student Manager");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
